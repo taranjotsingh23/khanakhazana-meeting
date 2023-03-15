8e5333b2a0e2428df5778b3c92fad78ded5b9a54
Unicorn GTBIT <unicorngtbit@gmail.com>
2023-03-15 21:47:43 +0530

    Updated Copy Link Part

--- src/pages/Room/index.jsx
+++ src/pages/Room/index.jsx
@@ -16,7 +16,7 @@ const RoomPage= () => {
             sharedLinks: [
                 {
                     name: 'Copy Link',
-                    url: `http://localhost:3000/room/${roomId}`
+                    url: `https://khanakhazana-meeting.vercel.app/room/${roomId}`
                 }
             ],
             scenario: {
